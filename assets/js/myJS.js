138f849efbfb04899f93aa1089ce7225c604c83e
Ross Nevin <rossnevin@Ross-MacBook-Pro-2.local>
2020-05-07 16:15:30 -0500

    initial commit

---
+++ assets/js/myJS.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $('a[href^="#"]').on("click", function (e) {
+        e.preventDefault();
+    
+    
+
+    var target = this.hash;
+    var $target = $(target);
+
+    //scorll and show hash
+
+    $('html, body').animate({
+        'scrollTop': $target.offset().top
+    }, 1000, 'swing',function() {
+            window.location.hash = target;
+     });
+    
+    });
+});
